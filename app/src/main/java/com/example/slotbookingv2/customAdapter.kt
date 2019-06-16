a1a4a045d1444220dca178c385f01cee66dca7b5
Nikhil Kumar Nishad <nikhilnishad279@gmail.com>
2019-06-16 23:10:16 +0530

    Merge branch 'master' of https://github.com/fineanmol/SlotBooking
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/slotbookingv2/UserHome.kt
    #	app/src/main/java/com/example/slotbookingv2/mentorHomeActivity.kt
    #	app/src/main/java/com/example/slotbookingv2/studentHomeActivity.kt
    #	app/src/main/res/layout/activity_user_home.xml

--- app/src/main/java/com/example/slotbookingv2/customAdapter.kt
+++ app/src/main/java/com/example/slotbookingv2/customAdapter.kt
@@ -8,16 +8,16 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class customAdapterc(val mCtx: Context, val layoutId: Int, val slotList: List<slotsData>) :
     ArrayAdapter<slotsData>(mCtx, layoutId, slotList) {
     val currentUser = FirebaseAuth.getInstance().currentUser
     val ref = FirebaseDatabase.getInstance().getReference("Slots")
-    //lateinit var name: TextView
-    //lateinit var dateslot: TextView
-    //lateinit var timeslot: TextView
-    //lateinit var bookbtn: Button
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
 
         val layoutInflater: LayoutInflater = LayoutInflater.from(mCtx)
@@ -38,81 +38,39 @@ class customAdapterc(val mCtx: Context, val layoutId: Int, val slotList: List<sl
 
         book.setOnClickListener {
             var id = slot.sid
-            ref.child(slot.generated_by).child(id).child("status").setValue("B")
-            Toast.makeText(mCtx, "Booked!" + id, Toast.LENGTH_LONG).show()
+            currentUser?.let { user ->
+                Toast.makeText(mCtx, user.email, Toast.LENGTH_LONG).show()
+                val userNameRef = ref.parent?.child("users")?.orderByChild("email")?.equalTo(user.email)
+                val eventListener = object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (!dataSnapshot.exists()) {
+                            //create new user
+                            Toast.makeText(mCtx, "User details not found", Toast.LENGTH_LONG).show()
+                        } else {
+                            for (e in dataSnapshot.children) {
+                                val employee = e.getValue(Data::class.java)
+                                var studentId = employee?.studentId
+                                var studentName = employee?.name
+                                var phone = employee?.number
+                                ref.child(slot.generated_by).child(id).child("studentNumber").setValue(phone)
+                                ref.child(slot.generated_by).child(id).child("reserved_by").setValue(studentName)
+                                ref.child(slot.generated_by).child(id).child("studentId").setValue(studentId)
+                                ref.child(slot.generated_by).child(id).child("status").setValue("B")
+                                Toast.makeText(mCtx, "Booked!" + id, Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                    }
+                }
+                userNameRef?.addListenerForSingleValueEvent(eventListener)
+
+            }
+
 
         }
-
-        /*deleteBtn.setOnClickListener {
-            deleteInfo(employee)
-        }*/
-
         return view
-    }
-
-    /*private fun updateInfo(employee: List<slotsData>) {
-
-        val builder = AlertDialog.Builder(mCtx)
-        builder.setTitle("Update Info")
-        val inflater = LayoutInflater.from(mCtx)
-        val view = inflater.inflate(R.layout.user_update, null)
-
-        val email = view.findViewById<EditText>(R.id.editemail)
-        val pass = view.findViewById<EditText>(R.id.editpass)
-        val name = view.findViewById<EditText>(R.id.nameupdate)
-        val number = view.findViewById<EditText>(R.id.numberupdate)
-        val dob = view.findViewById<EditText>(R.id.numberupdate)
-
-        email.setText(employee.email)
-        pass.setText(employee.pass)
-        name.setText(employee.name)
-        dob.setText(employee.dob)
-
-        builder.setView(view)
-
-        builder.setPositiveButton("Update", object : DialogInterface.OnClickListener {
-            override fun onClick(dialog: DialogInterface?, which: Int) {
-
-                val myDatabase = FirebaseDatabase.getInstance().getReference("users")
-
-                val email1 = email.text.toString().trim()
-                val pass2 = pass.text.toString().trim()
-                val name= name.text.toString().trim()
-                val number =number.text.toString().trim()
-                val dob = dob.text.toString().trim()
-
-                if (email1.isEmpty()) {
-                    email.error = "Please enter your email"
-                    return
-                }
-                if (pass2.isEmpty()) {
-                    pass.error = "Please enter your pass"
-                    return
-                }
-
-                val employee = Data(employee.id, email1, pass2,name,number,dob)
-                myDatabase.child(employee.id).setValue(employee)
-                Toast.makeText(mCtx, "Updated :) ", Toast.LENGTH_LONG).show()
-
-
-            }
-        })
-
-        builder.setNegativeButton("cancel", object : DialogInterface.OnClickListener {
-            override fun onClick(dialog: DialogInterface?, which: Int) {
-
-            }
-
-        })
-
-        val alert = builder.create()
-        alert.show()
-
-    }*/
-
-
-    private fun deleteInfo(employee: List<slotsData>) {
-
     }
 }
 
