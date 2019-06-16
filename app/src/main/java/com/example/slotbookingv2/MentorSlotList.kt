0a106a0dc49df216ad0c8c1495b5c5ea3a9dee1e
Anmol <anmol.agarwal2004@yahoo.com>
2019-06-16 23:37:25 +0530

    init

--- app/src/main/java/com/example/slotbookingv2/MentorSlotList.kt
+++ app/src/main/java/com/example/slotbookingv2/MentorSlotList.kt
@@ -2,11 +2,21 @@ package com.example.slotbookingv2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MentorSlotList : AppCompatActivity() {
 
+    var array = arrayOf("Melbourne", "Vienna", "Vancouver", "Toronto", "Calgary", "Adelaide", "Perth", "Auckland", "Helsinki", "Hamburg", "Munich", "New York", "Sydney", "Paris", "Cape Town", "Barcelona", "London", "Bangkok")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mentor_slot_list)
+        setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter(this,
+            R.layout.activity_mentor_slot_list, array)
+
+        val listView: ListView = findViewById(R.id.mentorslotlist)
+        listView.setAdapter(adapter)
     }
 }
