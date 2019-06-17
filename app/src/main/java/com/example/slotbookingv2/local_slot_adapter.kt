2bf333138dc42c3e7c8956e98b6878525e3218c8
Anmol <anmol.agarwal2004@yahoo.com>
2019-06-17 20:30:04 +0530

    CLoud Messaging messed up again

--- app/src/main/java/com/example/slotbookingv2/local_slot_adapter.kt
+++ app/src/main/java/com/example/slotbookingv2/local_slot_adapter.kt
@@ -1,16 +1,14 @@
 package com.example.slotbookingv2
 
 import android.content.Context
+import android.content.Intent
+import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
-import android.support.v4.content.LocalBroadcastManager
-import android.content.Intent
-import android.widget.Button
-import android.widget.Toast
 
 
 class local_slot_adapter(val mCtx: Context, val layoutId: Int, var local_slotList: MutableList<String>) :
@@ -43,7 +41,7 @@ class local_slot_adapter(val mCtx: Context, val layoutId: Int, var local_slotLis
         }
 
         val intent = Intent("custom-message")
-        //            intent.putExtra("quantity",Integer.parseInt(quantity.getText().toString()));
+        //intent.putExtra("quantity",Integer.parseInt(quantity.getText().toString()));
         intent.putExtra("quantity", local_slotList.toString())
         intent.putExtra("item", "Slot List")
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent)
--- app/src/main/java/com/example/slotbookingv2/mentorhomev2.kt
+++ app/src/main/java/com/example/slotbookingv2/mentorhomev2.kt
@@ -23,6 +23,7 @@ class mentorhomev2 : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     val userref = FirebaseDatabase.getInstance().getReference("users")
     val currentUser = FirebaseAuth.getInstance().currentUser
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mentorhomev2)
@@ -53,6 +54,26 @@ class mentorhomev2 : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         navView.setNavigationItemSelectedListener(this)
+
+
+        /*Firebase Messaging*/
+        /* FirebaseInstanceId.getInstance().instanceId
+             .addOnCompleteListener(OnCompleteListener { task ->
+                 if (!task.isSuccessful) {
+                     Log.w(TAG, "getInstanceId failed", task.exception)
+                     return@OnCompleteListener
+                 }
+
+                 // Get new Instance ID token
+                 val token = task.result?.token
+
+                 // Log and toast
+                 val msg = getString(R.string.msg_token_fmt, token)
+                 Log.d(TAG, msg)
+                 Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+             })*/
+        /*Firebase Messaging Ends*/
+
     }
 
     override fun onBackPressed() {
