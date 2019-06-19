87de7eb0183a765257d5f7091c5cb164deafbf60
Anmol <anmol.agarwal2004@yahoo.com>
2019-06-19 23:34:02 +0530

    AM PM bugs removed , Start and End time Constraint added,  and Ui Enhanced

--- app/src/main/java/com/example/slotbookingv2/MentorSlotList.kt
+++ app/src/main/java/com/example/slotbookingv2/MentorSlotList.kt
@@ -18,10 +18,10 @@ class MentorSlotList : AppCompatActivity() {
     lateinit var parts: MutableList<String>
     lateinit var listView: ListView
     var receiver: BroadcastReceiver? = null
-    lateinit var qty:String
-    lateinit var date:String
-    lateinit var stime:String
-    lateinit var etime:String
+    lateinit var qty: String
+    lateinit var date: String
+    lateinit var stime: String
+    lateinit var etime: String
     lateinit var ref: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -96,6 +96,7 @@ class MentorSlotList : AppCompatActivity() {
           
         }*/
     }
+
     var mMessageReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             // Get extra data included in the Intent
@@ -120,6 +121,7 @@ class MentorSlotList : AppCompatActivity() {
                 DialogInterface.OnClickListener { arg0, arg1 -> })
             .show()
     }
+
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(receiver)
@@ -164,36 +166,58 @@ class MentorSlotList : AppCompatActivity() {
                     // do something when the button is clicked
 
                     var parts1 = qty.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toMutableList()
+                    if (parts1.size > 1) {
 
 
-                    for (x in parts1) {
-                        Log.d("TAG1", x)
-                        date = x.split("$").last().toString().trim().replace("]]", "")
-                        stime = x.split("$").first().split("-").first().toString().trim().replace("[[", "")
-                        etime = x.split("$").first().split("-").last().toString().trim()
-                        addSlot(stime, etime, date)
-                        //Toast.makeText(this, stime+"-"+etime+" "+date, Toast.LENGTH_LONG).show()
-                    }
+                        for (x in parts1) {
+                            Log.d("TAG1", x)
+                            date = x.split("$").last().toString().trim().replace("]]", "")
+                            stime = x.split("$").first().split("-").first().toString().trim().replace("[[", "")
+                            etime = x.split("$").first().split("-").last().toString().trim()
+                            addSlot(stime, etime, date)
+                            //Toast.makeText(this, stime+"-"+etime+" "+date, Toast.LENGTH_LONG).show()
+                        }
 
-                    /*Alert Box*/
-                    val alertbox = AlertDialog.Builder(this)
-                        .setMessage("Do you want to Add More Slots?")
-                        .setPositiveButton("Yes", DialogInterface.OnClickListener { arg0, arg1 ->
-                            // do something when the button is clicked
-                            val intent = Intent(this, addSlotActivity::class.java)
-                            startActivity(intent)
-
-
-                        })
-                        .setNegativeButton("No", // do something when the button is clicked
-
-                            DialogInterface.OnClickListener { arg0, arg1 ->
-                                val intent = Intent(this, mentorhomev2::class.java)
+                        /*Alert Box*/
+                        val alertbox = AlertDialog.Builder(this)
+                            .setMessage("Do you want to Add More Slots?")
+                            .setPositiveButton("Yes", DialogInterface.OnClickListener { arg0, arg1 ->
+                                // do something when the button is clicked
+                                val intent = Intent(this, addSlotActivity::class.java)
                                 startActivity(intent)
-                            })
-                        .show()
-                    /*Alert Box*/
 
+
+                            })
+                            .setNegativeButton("No", // do something when the button is clicked
+
+                                DialogInterface.OnClickListener { arg0, arg1 ->
+                                    val intent = Intent(this, mentorhomev2::class.java)
+                                    startActivity(intent)
+                                })
+                            .show()
+                        /*Alert Box*/
+
+                    }
+                    if (parts1.size <= 1) {
+                        /*Alert Box*/
+                        val alertbox = AlertDialog.Builder(this)
+                            .setMessage("No slot present to be submit \n Generate slots again?")
+                            .setPositiveButton("Yes", DialogInterface.OnClickListener { arg0, arg1 ->
+                                // do something when the button is clicked
+                                val intent = Intent(this, addSlotActivity::class.java)
+                                startActivity(intent)
+
+
+                            })
+                            .setNegativeButton("No", // do something when the button is clicked
+
+                                DialogInterface.OnClickListener { arg0, arg1 ->
+                                    val intent = Intent(this, mentorhomev2::class.java)
+                                    startActivity(intent)
+                                })
+                            .show()
+                        /*Alert Box*/
+                    }
                 })
                 .setNegativeButton("No", // do something when the button is clicked
 
--- app/src/main/java/com/example/slotbookingv2/studentHomeActivityV2.kt
+++ app/src/main/java/com/example/slotbookingv2/studentHomeActivityV2.kt
@@ -6,7 +6,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
@@ -30,8 +29,16 @@ class UserHomeV2 : AppCompatActivity(), NavigationView.OnNavigationItemSelectedL
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            /* Snackbar.make(view, "Email your suggestion", Snackbar.LENGTH_LONG)
+                 .setAction("Action", null).show()*/
+            val intent = Intent(
+                Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto", "agarwal.anmol2004@gmail.com", null
+                )
+            )
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Report of Bugs,Improvements")
+            intent.putExtra(Intent.EXTRA_TEXT, android.R.id.message)
+            startActivity(Intent.createChooser(intent, "Choose an Email client :"))
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
