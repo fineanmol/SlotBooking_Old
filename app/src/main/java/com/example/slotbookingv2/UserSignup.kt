7f5779e5816b942880fec47d2a9dbd9d338fdcc4
Nikhil Kumar Nishad <nikhilnishad279@gmail.com>
2019-06-21 01:24:11 +0530

    User registration verification functionality added!

--- app/src/main/java/com/example/slotbookingv2/UserSignup.kt
+++ app/src/main/java/com/example/slotbookingv2/UserSignup.kt
@@ -2,18 +2,30 @@ package com.example.slotbookingv2
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_user_signup.*
+import java.util.concurrent.TimeUnit
+
 
 class UserSignup : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     lateinit var ref: DatabaseReference
+    var verificationId = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,7 +80,9 @@ class UserSignup : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            registerUser(email, password, namef, number, studentidf, status, user_type)
+            showCreateCategoryDialog(email, password, namef, number, studentidf, status, user_type)
+
+
 
         }
 
@@ -133,5 +147,150 @@ class UserSignup : AppCompatActivity() {
 
 
     }
+
+
+    fun showCreateCategoryDialog(
+        email: String,
+        password: String,
+        namef: String,
+        number: String,
+        studentidf: String,
+        status: String,
+        user_type: String
+    ) {
+        val context = this
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Phone Number Verification")
+
+        // https://stackoverflow.com/questions/10695103/creating-custom-alertdialog-what-is-the-root-view
+        // Seems ok to inflate view with null rootView
+        val view = layoutInflater.inflate(R.layout.dialog_otp_verification, null)
+
+        val categoryEditText = view.findViewById(R.id.editOTP) as EditText
+        val vfbutton = view.findViewById<Button>(R.id.get_otp_btn)
+        val vreg = view.findViewById<Button>(R.id.vregister)
+
+
+
+        vfbutton.setOnClickListener {
+            Log.d("xcv", "clicked")
+            var phone = "+91" + mobile.text.toString().trim()
+
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phone, // Phone number to verify
+                60, // Timeout duration
+                TimeUnit.SECONDS, // Unit of timeout
+                this, // Activity (for callback binding)
+                callbacks
+            ) // OnVerificationStateChangedCallbacks
+        }
+
+
+        vreg.setOnClickListener {
+            val code = editOTP.text.toString()
+            val credential = PhoneAuthProvider.getCredential(verificationId!!, code)
+            mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("TAG", "signInWithCredential:success")
+                        registerUser(email, password, namef, number, studentidf, status, user_type)
+
+                        val user = task.result?.user
+                        // ...
+                    } else {
+                        // Sign in failed, display a message and update the UI
+                        Log.w("TAG", "signInWithCredential:failure", task.exception)
+                        if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                            // The verification code entered was invalid
+                        }
+                    }
+                }
+        }
+
+
+
+        builder.setView(view);
+
+        builder.show();
+    }
+
+    val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+            Log.d("TAG", "onVerificationCompleted:$credential")
+
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+            Log.w("TAG", "onVerificationFailed", e)
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+                // ...
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+                // ...
+            }
+
+            // Show a message and update the UI
+            // ...
+        }
+
+        override fun onCodeSent(
+            verificationId: String?,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            Log.d("TAG", "onCodeSent:" + verificationId!!)
+
+            // Save verification ID and resending token so we can use them later
+            //storedVerificationId = verificationId
+            //resendToken = token
+
+            // ...
+        }
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithCredential:success")
+                    val email = u_r_email.text.toString().trim()
+                    val password = u_r_pass.text.toString().trim()
+                    val namef = name.text.toString().trim()
+                    val number = mobile.text.toString().trim()
+                    val studentidf = studentid.text.toString().trim()
+                    val status = ("NB")
+                    val user_type = ("S")
+                    registerUser(email, password, namef, number, studentidf, status, user_type)
+
+
+                    val user = task.result?.user
+                    // ...
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w("TAG", "signInWithCredential:failure", task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                }
+            }
+    }
+
+
 
 }
